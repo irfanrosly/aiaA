f0171623b681cd91883c563f1897de20e164b00a
Irfan Rosly <irfanrosly@gmail.com>
2022-10-16 19:32:56 +0800

    feat: mock data and flatlist

--- App.tsx
+++ App.tsx
@@ -8,33 +8,79 @@
  * @format
  */
 
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
-import { Dimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  View,
+  Dimensions,
+} from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 import { Button, Text } from './app/components/atom';
 import { Card } from './app/components/molecules';
+import axios from 'axios';
+
+const mockData = require('./app/mocks/apiData.json');
+console.log('mockData', mockData);
 
 const App = () => {
-  const width = Dimensions.get('window').width;
+  const [data, setData] = useState(mockData);
+
+  const changeOrder = () => {
+    const newData = data.sort((a, b) => a.Name < b.Name);
+    console.log('new', newData);
+    // setData(newData);
+  };
+  // const test = () => {
+  //   axios.get('http://webcode.me').then(resp => {
+  //     console.log(resp.data);
+  //   });
+  // };
+  const fetchData = async () => {
+    // const response = await axios.get(
+    //   'https://tastedive.com/api/similar?apiKey=443477-AIAAsses-W51EUAVP&q=red+hot+chili+peppers%2C+pulp+fiction',
+    // );
+    // console.log('toi ', response);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const { width, height } = Dimensions.get('window');
+
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
+  const renderItem = ({ item }) => <Item title={item.Name} />;
 
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ backgroundColor: 'blue', flex: 1 }}>
         <Carousel
           loop
           width={width}
-          height={width}
+          height={height * 0.4}
           autoPlay={true}
           data={[...new Array(6).keys()]}
           // scrollAnimationDuration={1000}
-          onSnapToItem={index => console.log('current index:', index)}
+          // onSnapToItem={index => console.log('current index:', index)}
           renderItem={({ index }) => <Card id={index} />}
+          style={{ backgroundColor: 'yellow' }}
         />
-        <Button title="Hello World" />
-        <Text>Welcome to the Jungle!</Text>
-      </ScrollView>
+        <View style={{ flex: 0.6 }}>
+          <Button title="Change Order" onPress={() => changeOrder()} />
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
